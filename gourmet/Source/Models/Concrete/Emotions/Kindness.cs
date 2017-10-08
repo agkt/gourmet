07ab3dda6374cf679b705df0367044d6c31c2fa1
Adel Khakimov <adel.khakimov@gmail.com>
2017-10-08 16:27:12 +0300

    added emotions implementation

---
+++ gourmet/Source/Models/Concrete/Emotions/Kindness.cs
@@ -0,0 +1,28 @@
+﻿using gourmet.Source.Models.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gourmet.Source.Models.Concrete.Emotions
+{
+    public class Kindness : IEmotion
+    {
+        public string Name => "Доброжелательность";
+        public bool? IsPositive => true;
+
+        public override bool Equals(object obj)
+        {
+            return Name == ((IEmotion)obj).Name;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+}
