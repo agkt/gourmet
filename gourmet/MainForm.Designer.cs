04d7ff753ebd8a9e479536d608367c87b1189382
rmirzojonov <ravshanjonmir@gmail.com>
2017-10-21 21:50:32 +0300

    Edited UI

--- gourmet/MainForm.Designer.cs
+++ gourmet/MainForm.Designer.cs
@@ -28,20 +28,294 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.LblStatus = new System.Windows.Forms.Label();
+            this.BtnOnion = new System.Windows.Forms.Button();
+            this.BtnAvocado = new System.Windows.Forms.Button();
+            this.BtnTomato = new System.Windows.Forms.Button();
+            this.BtnChilli = new System.Windows.Forms.Button();
+            this.BtnOrange = new System.Windows.Forms.Button();
+            this.BtnChocolate = new System.Windows.Forms.Button();
+            this.BtnFlour = new System.Windows.Forms.Button();
+            this.BtnMacaroni = new System.Windows.Forms.Button();
+            this.BtnBanana = new System.Windows.Forms.Button();
+            this.LBoxIngredients = new System.Windows.Forms.ListBox();
+            this.CONTEXT = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.BtnRedMeat = new System.Windows.Forms.Button();
+            this.BtnSalmon = new System.Windows.Forms.Button();
+            this.BtnEgg = new System.Windows.Forms.Button();
+            this.BtnSausage = new System.Windows.Forms.Button();
+            this.BtnWalnuts = new System.Windows.Forms.Button();
+            this.BtnVegetableOil = new System.Windows.Forms.Button();
+            this.BtnFeed = new System.Windows.Forms.Button();
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.CONTEXT.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // LblStatus
+            // 
+            this.LblStatus.AutoSize = true;
+            this.LblStatus.Location = new System.Drawing.Point(405, 349);
+            this.LblStatus.Name = "LblStatus";
+            this.LblStatus.Size = new System.Drawing.Size(56, 17);
+            this.LblStatus.TabIndex = 1;
+            this.LblStatus.Text = "[Status]";
+            // 
+            // BtnOnion
+            // 
+            this.BtnOnion.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnOnion.Location = new System.Drawing.Point(15, 12);
+            this.BtnOnion.Name = "BtnOnion";
+            this.BtnOnion.Size = new System.Drawing.Size(123, 103);
+            this.BtnOnion.TabIndex = 2;
+            this.BtnOnion.Tag = "Onion";
+            this.BtnOnion.UseVisualStyleBackColor = true;
+            this.BtnOnion.Click += new System.EventHandler(this.BtnOnion_Click);
+            // 
+            // BtnAvocado
+            // 
+            this.BtnAvocado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnAvocado.Location = new System.Drawing.Point(960, 121);
+            this.BtnAvocado.Name = "BtnAvocado";
+            this.BtnAvocado.Size = new System.Drawing.Size(123, 103);
+            this.BtnAvocado.TabIndex = 3;
+            this.BtnAvocado.Tag = "Avocado";
+            this.BtnAvocado.UseVisualStyleBackColor = true;
+            // 
+            // BtnTomato
+            // 
+            this.BtnTomato.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnTomato.Location = new System.Drawing.Point(15, 121);
+            this.BtnTomato.Name = "BtnTomato";
+            this.BtnTomato.Size = new System.Drawing.Size(123, 103);
+            this.BtnTomato.TabIndex = 4;
+            this.BtnTomato.Tag = "Tomato";
+            this.BtnTomato.UseVisualStyleBackColor = true;
+            // 
+            // BtnChilli
+            // 
+            this.BtnChilli.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnChilli.Location = new System.Drawing.Point(144, 121);
+            this.BtnChilli.Name = "BtnChilli";
+            this.BtnChilli.Size = new System.Drawing.Size(123, 103);
+            this.BtnChilli.TabIndex = 5;
+            this.BtnChilli.Tag = "Chilli";
+            this.BtnChilli.UseVisualStyleBackColor = true;
+            // 
+            // BtnOrange
+            // 
+            this.BtnOrange.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnOrange.Location = new System.Drawing.Point(144, 12);
+            this.BtnOrange.Name = "BtnOrange";
+            this.BtnOrange.Size = new System.Drawing.Size(123, 103);
+            this.BtnOrange.TabIndex = 6;
+            this.BtnOrange.Tag = "Orange";
+            this.BtnOrange.UseVisualStyleBackColor = true;
+            // 
+            // BtnChocolate
+            // 
+            this.BtnChocolate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnChocolate.Location = new System.Drawing.Point(328, 12);
+            this.BtnChocolate.Name = "BtnChocolate";
+            this.BtnChocolate.Size = new System.Drawing.Size(123, 103);
+            this.BtnChocolate.TabIndex = 7;
+            this.BtnChocolate.Tag = "MilkChocolate";
+            this.BtnChocolate.UseVisualStyleBackColor = true;
+            // 
+            // BtnFlour
+            // 
+            this.BtnFlour.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnFlour.Location = new System.Drawing.Point(457, 12);
+            this.BtnFlour.Name = "BtnFlour";
+            this.BtnFlour.Size = new System.Drawing.Size(123, 103);
+            this.BtnFlour.TabIndex = 8;
+            this.BtnFlour.Tag = "Flour";
+            this.BtnFlour.UseVisualStyleBackColor = true;
+            // 
+            // BtnMacaroni
+            // 
+            this.BtnMacaroni.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnMacaroni.Location = new System.Drawing.Point(328, 121);
+            this.BtnMacaroni.Name = "BtnMacaroni";
+            this.BtnMacaroni.Size = new System.Drawing.Size(123, 103);
+            this.BtnMacaroni.TabIndex = 9;
+            this.BtnMacaroni.Tag = "Macaroni";
+            this.BtnMacaroni.UseVisualStyleBackColor = true;
+            // 
+            // BtnBanana
+            // 
+            this.BtnBanana.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnBanana.Location = new System.Drawing.Point(457, 121);
+            this.BtnBanana.Name = "BtnBanana";
+            this.BtnBanana.Size = new System.Drawing.Size(123, 103);
+            this.BtnBanana.TabIndex = 10;
+            this.BtnBanana.Tag = "Banana";
+            this.BtnBanana.UseVisualStyleBackColor = true;
+            // 
+            // LBoxIngredients
+            // 
+            this.LBoxIngredients.ContextMenuStrip = this.CONTEXT;
+            this.LBoxIngredients.FormattingEnabled = true;
+            this.LBoxIngredients.ItemHeight = 16;
+            this.LBoxIngredients.Location = new System.Drawing.Point(15, 349);
+            this.LBoxIngredients.Name = "LBoxIngredients";
+            this.LBoxIngredients.Size = new System.Drawing.Size(384, 324);
+            this.LBoxIngredients.TabIndex = 11;
+            this.LBoxIngredients.MouseUp += new System.Windows.Forms.MouseEventHandler(this.LBoxIngredients_MouseUp);
+            // 
+            // CONTEXT
+            // 
+            this.CONTEXT.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.CONTEXT.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem});
+            this.CONTEXT.Name = "CONTEXT";
+            this.CONTEXT.Size = new System.Drawing.Size(123, 28);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(122, 24);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // BtnRedMeat
+            // 
+            this.BtnRedMeat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnRedMeat.Location = new System.Drawing.Point(643, 12);
+            this.BtnRedMeat.Name = "BtnRedMeat";
+            this.BtnRedMeat.Size = new System.Drawing.Size(123, 103);
+            this.BtnRedMeat.TabIndex = 12;
+            this.BtnRedMeat.Tag = "RedMeat";
+            this.BtnRedMeat.UseVisualStyleBackColor = true;
+            // 
+            // BtnSalmon
+            // 
+            this.BtnSalmon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnSalmon.Location = new System.Drawing.Point(772, 12);
+            this.BtnSalmon.Name = "BtnSalmon";
+            this.BtnSalmon.Size = new System.Drawing.Size(123, 103);
+            this.BtnSalmon.TabIndex = 13;
+            this.BtnSalmon.Tag = "Salmon";
+            this.BtnSalmon.UseVisualStyleBackColor = true;
+            // 
+            // BtnEgg
+            // 
+            this.BtnEgg.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnEgg.Location = new System.Drawing.Point(643, 121);
+            this.BtnEgg.Name = "BtnEgg";
+            this.BtnEgg.Size = new System.Drawing.Size(123, 103);
+            this.BtnEgg.TabIndex = 14;
+            this.BtnEgg.Tag = "Egg";
+            this.BtnEgg.UseVisualStyleBackColor = true;
+            // 
+            // BtnSausage
+            // 
+            this.BtnSausage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnSausage.Location = new System.Drawing.Point(772, 121);
+            this.BtnSausage.Name = "BtnSausage";
+            this.BtnSausage.Size = new System.Drawing.Size(123, 103);
+            this.BtnSausage.TabIndex = 15;
+            this.BtnSausage.Tag = "Sausage";
+            this.BtnSausage.UseVisualStyleBackColor = true;
+            // 
+            // BtnWalnuts
+            // 
+            this.BtnWalnuts.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnWalnuts.Location = new System.Drawing.Point(960, 12);
+            this.BtnWalnuts.Name = "BtnWalnuts";
+            this.BtnWalnuts.Size = new System.Drawing.Size(123, 103);
+            this.BtnWalnuts.TabIndex = 16;
+            this.BtnWalnuts.Tag = "Walnuts";
+            this.BtnWalnuts.UseVisualStyleBackColor = true;
+            // 
+            // BtnVegetableOil
+            // 
+            this.BtnVegetableOil.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BtnVegetableOil.Location = new System.Drawing.Point(1089, 12);
+            this.BtnVegetableOil.Name = "BtnVegetableOil";
+            this.BtnVegetableOil.Size = new System.Drawing.Size(123, 103);
+            this.BtnVegetableOil.TabIndex = 17;
+            this.BtnVegetableOil.Tag = "VegetableOil";
+            this.BtnVegetableOil.UseVisualStyleBackColor = true;
+            // 
+            // BtnFeed
+            // 
+            this.BtnFeed.Location = new System.Drawing.Point(15, 296);
+            this.BtnFeed.Name = "BtnFeed";
+            this.BtnFeed.Size = new System.Drawing.Size(123, 47);
+            this.BtnFeed.TabIndex = 19;
+            this.BtnFeed.Text = "Feed";
+            this.BtnFeed.UseVisualStyleBackColor = true;
+            this.BtnFeed.Click += new System.EventHandler(this.BtnFeed_Click);
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(144, 296);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(123, 47);
+            this.BtnClear.TabIndex = 20;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(703, 460);
+            this.ClientSize = new System.Drawing.Size(1280, 685);
+            this.Controls.Add(this.BtnClear);
+            this.Controls.Add(this.BtnFeed);
+            this.Controls.Add(this.BtnVegetableOil);
+            this.Controls.Add(this.BtnWalnuts);
+            this.Controls.Add(this.BtnSausage);
+            this.Controls.Add(this.BtnEgg);
+            this.Controls.Add(this.BtnSalmon);
+            this.Controls.Add(this.BtnRedMeat);
+            this.Controls.Add(this.LBoxIngredients);
+            this.Controls.Add(this.BtnBanana);
+            this.Controls.Add(this.BtnMacaroni);
+            this.Controls.Add(this.BtnFlour);
+            this.Controls.Add(this.BtnChocolate);
+            this.Controls.Add(this.BtnOrange);
+            this.Controls.Add(this.BtnChilli);
+            this.Controls.Add(this.BtnTomato);
+            this.Controls.Add(this.BtnAvocado);
+            this.Controls.Add(this.BtnOnion);
+            this.Controls.Add(this.LblStatus);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Gourmet bot";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.CONTEXT.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Label LblStatus;
+        private System.Windows.Forms.Button BtnOnion;
+        private System.Windows.Forms.Button BtnAvocado;
+        private System.Windows.Forms.Button BtnTomato;
+        private System.Windows.Forms.Button BtnChilli;
+        private System.Windows.Forms.Button BtnOrange;
+        private System.Windows.Forms.Button BtnChocolate;
+        private System.Windows.Forms.Button BtnFlour;
+        private System.Windows.Forms.Button BtnMacaroni;
+        private System.Windows.Forms.Button BtnBanana;
+        private System.Windows.Forms.ListBox LBoxIngredients;
+        private System.Windows.Forms.Button BtnRedMeat;
+        private System.Windows.Forms.Button BtnSalmon;
+        private System.Windows.Forms.Button BtnEgg;
+        private System.Windows.Forms.Button BtnSausage;
+        private System.Windows.Forms.Button BtnWalnuts;
+        private System.Windows.Forms.Button BtnVegetableOil;
+        private System.Windows.Forms.ContextMenuStrip CONTEXT;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.Button BtnFeed;
+        private System.Windows.Forms.Button BtnClear;
     }
 }
 
