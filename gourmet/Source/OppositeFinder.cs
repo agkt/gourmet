c3ee1a332bd7107911c5ff20396d00446ea11405
Adel Khakimov <adel.khakimov@gmail.com>
2017-11-11 18:27:41 +0300

    added ingredientList & Food compapbility table

---
+++ gourmet/Source/OppositeFinder.cs
@@ -0,0 +1,37 @@
+﻿using gourmet.Source.Models.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gourmet.Source
+{
+    public class OppositeFinder
+    {
+        public OppositeFinder()
+        {
+            //Dictionary<IIngredient> onionPositives
+            Matrix = new int[(int)(IngredientList.Walnuts + 1), (int)(IngredientList.Walnuts + 1)]
+            {
+                { 0, 0, 1, -1, -1, -1, -1, 1, 0, -1, -1, -1, 0, 0, 0},
+                { 0, 0, 1, -1, -1, -1, -1, 1, 0, -1, -1, -1, 0, 0, 0},
+                { 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+                { -1, -1, 1, 0, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1, -1},
+                { -1, -1, 1, -1, 0, 0, -1, 1, -1, -1, -1, -1, -1, 1, 0},
+                { -1, -1, 1, -1, 0, 0, -1, 1, -1, -1, -1, -1, -1, 1, 0},
+                { -1, -1, 1, -1, -1, -1, 0, 1, -1, -1, -1, -1, -1, -1, -1},
+                { 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1},
+                { 0, 0, 1, -1, -1, -1, -1, 1, 0, -1, -1, -1, 0, 1, 1},
+                { -1, -1, 1, -1, -1, -1, -1, 1, -1, 0, 0, 0, -1, -1, -1},
+                { -1, -1, 1, -1, -1, -1, -1, 1, -1, 0, 0, 0, -1, -1, -1},
+                { -1, -1, 1, -1, -1, -1, -1, 1, -1, 0, 0, 0, -1, -1, -1},
+                { 0, 0, 1, -1, -1, -1, -1, 1, 0, -1, -1, -1, 0, 1, 1},
+                { 0, 0, 1, -1, 1, 1, -1, 1, 1, -1, -1, -1, 1, 0, 1},
+                { 0, 0, 1, -1, 0, 0, -1, 1, 1, -1, -1, -1, 1, 1, 0}
+            };
+        }
+
+        public int[,] Matrix { get; set; }
+    }
+}
