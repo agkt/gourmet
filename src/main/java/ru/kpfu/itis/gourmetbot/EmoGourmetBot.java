2e56d190148fd0b1640011cfe7e4c1e0b5df782e
Adel Khakimov <adel.khakimov@gmail.com>
2017-11-23 22:51:36 +0300

    telegram bot init

---
+++ src/main/java/ru/kpfu/itis/gourmetbot/EmoGourmetBot.java
@@ -0,0 +1,39 @@
+package ru.kpfu.itis.gourmetbot;
+
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class EmoGourmetBot extends TelegramLongPollingBot{
+
+
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            sendMsg(message, message.getText());
+        }
+    }
+
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId());
+//        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText("_" + text + "_");
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getBotUsername() {
+        return "ru.kpfu.itis.gourmetbot.EmoGourmetBot";
+    }
+
+    public String getBotToken() {
+        return "458682047:AAH2jnWgg0kCJ5SpLFg5AQIiAKBGEGbW-SQ";
+    }
+}
